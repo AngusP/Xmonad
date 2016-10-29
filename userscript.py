f9067575dfbfa675860c76b50f534cf21817c47d
AngusP <a7cp3ar50n@gmail.com>
2016-10-29 12:59:54 +0100

    Userscripte beter, has real names now

--- userscript.py
+++ userscript.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from __future__ import print_function
 import subprocess
+import re
 
 selfname = subprocess.check_output(['whoami']).strip(" \n\r")
 users = []
@@ -13,11 +14,20 @@ alert_colour = "<fc=#FF0000>"
 normal_colour = "<fc=#CCC>"
 end_colour = "</fc>"
 
+finger_re = re.compile("(Name: )([a-zA-Z ]+)")
+
 print(normal_colour + " (" + str(len(users)) + ")" + end_colour, end="")
 for user in users:
     if user[0] == selfname:
         #print(normal_colour + " " + user[-1:][0] + end_colour + "    ", end="")
         pass
     else:
-        print(alert_colour + "  --  " + user[0][:8] + "@" + user[-1:][0] + end_colour, end="")
+        try:
+            actual_name = finger_re.search(subprocess.check_output(['finger',user[0]]))\
+                                   .group(2)\
+                                   .lower()\
+                                   .replace(' ','')
+        except Exception:
+            actaul_name = user[0]
+        print(alert_colour + "  --  " + actual_name[:12] + "@" + user[-1:][0] + end_colour, end="")
 
